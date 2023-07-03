c92bd998f7aa2877095d15df5648633def9dedfd
21213908it <99856461+21213908it@users.noreply.github.com>
2023-07-03 15:52:20 +0530

    commit

---
+++ backend/products.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+        id:1,
+        name:"Panedol Tablets 500mg",
+        desc:"paracetamol",
+        price:"500",
+        image:"https://www.healthguard.lk/pub/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/g/l/glx-pt-144-01_800x.jpg",
+    },
+
+    {
+        id:2,
+        name:"Sustegen Vanilla 450g",
+        desc:"Nutritional supplement",
+        price:"3500",
+        image:"https://www.healthguard.lk/pub/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/1/6/16.jpg",
+
+    },
+
+    {
+        id:3,
+        name:"Link Samahan 10pk",
+        desc:"Herbal Drink",
+        price:"500",
+        image:" https://www.healthguard.lk/pub/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/s/a/samahan-instant-peyava-10-box.jpg",
+
+    },
+
+    {
+        id:4,
+        name:"Surgical Spirit",
+        desc:"Isoprpyl Alcohol",
+        price:"100",
+        image:"https://www.healthguard.lk/pub/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/1/3/137_1.jpg",
+
+    },
+];
+
+module.exports = products;
